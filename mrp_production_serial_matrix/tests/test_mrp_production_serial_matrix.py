08d95352d2dcc0b2408579477286bd92d68bae0a
Tom <tblauwendraat@therp.nl>
2026-04-08 14:27:26 +0200

    [ADD] mrp_production_serial_matrix: more simple test case

--- mrp_production_serial_matrix/tests/test_mrp_production_serial_matrix.py
+++ mrp_production_serial_matrix/tests/test_mrp_production_serial_matrix.py
@@ -1,9 +1,13 @@
 # Copyright 2021 ForgeFlow S.L. (http://www.forgeflow.com)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
+import logging
+
 from odoo.exceptions import UserError
 from odoo.tests import Form
 from odoo.tests.common import SavepointCase
+
+_logger = logging.getLogger(__name__)
 
 
 class TestMrpProductionSerialMatrix(SavepointCase):
@@ -26,6 +30,13 @@ class TestMrpProductionSerialMatrix(SavepointCase):
         cls.final_product = cls.product_obj.create(
             {
                 "name": "Finished Product tracked by Serial Numbers",
+                "type": "product",
+                "tracking": "serial",
+            }
+        )
+        cls.final_product_simple = cls.product_obj.create(
+            {
+                "name": "Finished Product tracked by Serial Numbers (simple version)",
                 "type": "product",
                 "tracking": "serial",
             }
@@ -118,6 +129,21 @@ class TestMrpProductionSerialMatrix(SavepointCase):
                 "product_qty": 1.0,
             }
         )
+        # BoM 2 (simple BoM, only no tracking products)
+        cls.bom_2 = cls.bom_obj.create(
+            {
+                "product_tmpl_id": cls.final_product_simple.product_tmpl_id.id,
+                "product_id": cls.final_product_simple.id,
+                "product_qty": 1.0,
+            }
+        )
+        cls.bom_line_obj.create(
+            {
+                "bom_id": cls.bom_2.id,
+                "product_id": cls.component_4_no_track.id,
+                "product_qty": 4.0,
+            }
+        )
 
     @classmethod
     def _create_serial_number(cls, product, name, qty=1.0):
@@ -140,12 +166,12 @@ class TestMrpProductionSerialMatrix(SavepointCase):
         return lot
 
     @classmethod
-    def _create_mo(cls, qty):
+    def _create_mo(cls, product, qty):
         production_form = Form(cls.mo_obj)
-        production_form.product_id = cls.final_product
-        production_form.bom_id = cls.bom_1
+        production_form.product_id = product
+        production_form.bom_id = product.bom_ids[0]
         production_form.product_qty = qty
-        production_form.product_uom_id = cls.final_product.uom_id
+        production_form.product_uom_id = product.uom_id
         production_1 = production_form.save()
         production_1.action_confirm()
         production_1.action_assign()
@@ -167,7 +193,7 @@ class TestMrpProductionSerialMatrix(SavepointCase):
         - 1 tracked by lots.
         - 1 untracked.
         """
-        production_1 = self._create_mo(3.0)
+        production_1 = self._create_mo(self.final_product, 3.0)
         self.assertEqual(production_1.state, "confirmed")
         # Start matrix:
         wizard_form = Form(
@@ -284,3 +310,38 @@ class TestMrpProductionSerialMatrix(SavepointCase):
         mo_3 = mos.filtered(lambda mo: not mo.lot_producing_id)
         self.assertEqual(mo_3.state, "confirmed")
         self.assertEqual(mo_3.product_qty, 1.0)
+
+    def test_02_process_matrix_simple(self):
+        """Simple test:
+        - 1 product that is serially tracked
+        - Consisting of 1 component that is not serially tracked
+        - Produce 4 times
+        """
+        product = self.final_product_simple
+        production_1 = self._create_mo(product, 4.0)
+        self.assertEqual(production_1.state, "confirmed")
+        serial1 = self._create_serial_number(product, "ABC201", qty=0)
+        serial2 = self._create_serial_number(product, "ABC202", qty=0)
+        serial3 = self._create_serial_number(product, "ABC203", qty=0)
+        serial4 = self._create_serial_number(product, "ABC204", qty=0)
+
+        wizard = self.wiz_obj.with_context(
+            active_id=production_1.id, active_model="mrp.production"
+        ).create({})
+        lots = serial1 + serial2 + serial3 + serial4
+        self.assertEqual(len(lots), 4)
+        wizard.finished_lot_ids = lots
+        self.assertEqual(wizard.product_id, self.final_product_simple)
+        self.assertEqual(wizard.finished_lot_ids, lots)
+        wizard.button_validate()
+
+        mos = production_1.procurement_group_id.mrp_production_ids
+        self.assertEqual(len(mos), 4)
+        mo_1 = mos.filtered(lambda mo: mo.lot_producing_id == serial1)
+        self.assertEqual(mo_1.state, "done")
+        mo_2 = mos.filtered(lambda mo: mo.lot_producing_id == serial2)
+        self.assertEqual(mo_2.state, "done")
+        mo_3 = mos.filtered(lambda mo: mo.lot_producing_id == serial3)
+        self.assertEqual(mo_3.state, "done")
+        mo_4 = mos.filtered(lambda mo: mo.lot_producing_id == serial4)
+        self.assertEqual(mo_4.state, "done")
